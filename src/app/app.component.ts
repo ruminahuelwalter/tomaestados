5be668731da896df0647657cd3638b5a17ad5ec8
ruminahuelwalter <ruminahuelwalter@gmail.com>
2026-01-04 00:56:33 -0300

    add: routes, title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DiscarComercialComponent } from "./components/discar-comercial/discar-comercial.component";
-import { ConvertirCvsTextoPaparseComponent } from "./components/temetra-comercial/temetra-comercial.component";
-import { GenerarKmlComponent } from "./components/generar-kml/generar-kml.component";
+import { NavBarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DiscarComercialComponent, ConvertirCvsTextoPaparseComponent, GenerarKmlComponent],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'tomaestadosweb';
+  title = 'tomaestadostools';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DiscarComercialComponent } from './components/discar-comercial/discar-comercial.component';
+import { TemetraComercialComponent } from './components/temetra-comercial/temetra-comercial.component';
+import { GenerarKmlComponent } from './components/generar-kml/generar-kml.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'discar', component: DiscarComercialComponent },
+  { path: 'temetra', component: TemetraComercialComponent },
+  { path: 'mapa', component: GenerarKmlComponent },
+  { path: '**', redirectTo: '' }
+];
